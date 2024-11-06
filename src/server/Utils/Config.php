0959727639c95af61e92be6fb820ce07a65ab81d
John Kraczek <John.kraczek@gmail.com>
2024-11-06 07:27:25 -0700

    Update the config location

--- src/server/Utils/Config.php
+++ src/server/Utils/Config.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace YDTBWidgets\Utils;
-class config
+class Config
 {
     public static function get($key = ''): object|string
     {
-        $plugin_file = plugins_url('', dirname(__FILE__, 3));
+        $plugin_file = dirname(__FILE__, 3);
 
         $config = [
-            'plugin_file' => $plugin_file,
+            'plugin_file' => $plugin_file . "ydtb-elements.php",
             'plugin_basename' => plugin_basename($plugin_file),
             'plugin_path' => plugin_dir_path($plugin_file),
             'plugin_url' => plugin_dir_url($plugin_file)
