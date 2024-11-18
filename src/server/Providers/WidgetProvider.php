3a9c06da8c2618193459aaef5f0b338ea1c46f9a
John Kraczek <John.kraczek@gmail.com>
2024-11-17 20:17:45 -0700

    Added Keys Widget to Plugin

--- src/server/Providers/WidgetProvider.php
+++ src/server/Providers/WidgetProvider.php
@@ -4,11 +4,18 @@ namespace YDTBWidgets\Providers;
 
 use YDTBWidgets\Interfaces\Provider;
 use YDTBWidgets\Modules\CodeHighlight;
+use YDTBWidgets\Modules\KeyboardWidget;
+use \Elementor\Plugin;
 
 class WidgetProvider implements Provider
 {
     public function register(): void
     {
+        add_action('init', [$this, 'loadWidgets']);
+    }
+    public function loadWidgets(): void
+    {
         new CodeHighlight();
+        Plugin::instance()->widgets_manager->register(new KeyboardWidget());
     }
 }
