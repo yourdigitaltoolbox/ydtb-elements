e49a15fbc64ff3892050e8df48d3e71fa77b3ea4
John Kraczek <john@yourdigitaltoolbox.com>
2025-04-17 10:16:10 -0600

    Fix widget loading action

--- src/server/Providers/WidgetProvider.php
+++ src/server/Providers/WidgetProvider.php
@@ -5,7 +5,6 @@ namespace YDTBWidgets\Providers;
 use YDTBWidgets\Interfaces\Provider;
 use YDTBWidgets\Modules\CodeHighlight;
 use YDTBWidgets\Modules\KeyboardWidget;
-use YDTBWidgets\Modules\CodeGateway;
 use YDTBWidgets\Modules\PeerTubeVideo;
 use Elementor\Plugin;
 
@@ -13,13 +12,12 @@ class WidgetProvider implements Provider
 {
     public function register(): void
     {
-        add_action('init', [$this, 'loadWidgets']);
+        add_action('elementor/widgets/register', [$this, 'loadWidgets'], 1);
     }
     public function loadWidgets(): void
     {
         new CodeHighlight();
         Plugin::instance()->widgets_manager->register(new KeyboardWidget());
-        // Plugin::instance()->widgets_manager->register(new CodeGateway());
         Plugin::instance()->widgets_manager->register(new PeerTubeVideo());
     }
 }
